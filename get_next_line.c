8ee8ed41f40691853424657776a5db1990c9f8b9
Prinda Ruangdej <pruangde@z1t1c1.42bangkok.com>
2022-03-07 19:56:18 +0700

    add file

--- get_next_line.c
+++ get_next_line.c
@@ -6,15 +6,32 @@
 /*   By: pruangde <pruangde@student.42bangkok.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 22:26:05 by pruangde          #+#    #+#             */
-/*   Updated: 2022/03/06 18:19:18 by pruangde         ###   ########.fr       */
+/*   Updated: 2022/03/07 19:54:31 by pruangde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
 char	*get_next_line(int fd)
 {
+	int		rfd;
 	char	*buf;
 	char	*str;
 
-	buf = (char *)malloc(
-	while (
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buf = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buf)
+		return (NULL);
+	rfd = read(fd, buf, BUFFER_SIZE);
+	if (rfd == -1)
+		return (NULL);
+	buf[BUFFER_SIZE
+	str = ft_strjoin(str, buf);
+
+	while (read(fd, buf, BUFFER_SIZE) > 0)
+	{
+		
+
+	}
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,42 @@
 /*   By: pruangde <pruangde@student.42bangkok.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 22:27:38 by pruangde          #+#    #+#             */
-/*   Updated: 2022/03/04 22:28:18 by pruangde         ###   ########.fr       */
+/*   Updated: 2022/03/07 19:38:20 by pruangde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	size_t	i;
+	size_t	j:
+	size_t	len;
+	char	*str;
+
+	if (!s1 && !s2)
+		return (NULL);
+	else
+		len = ft_strlen(s1) + ft_strlen(s2);
+	str = (char *)ft_calloc((len + 1), sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[j])
+		str[i++] = s1[j++];
+	j = 0;
+	while (s2[j])
+		str[i++] = s2[j++];
+	return (str);
+}
